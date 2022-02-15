97e3ec916c5c187a9cb2699e306f1904db1c053b
RafaBSilvaDev <RAFAEL.BSILVA@bandtec.com.br>
2022-02-15 09:18:20 -0300

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { UsersComponent } from './users/users.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { useAnimation } from '@angular/animations';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'users',
+    component: UsersComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
